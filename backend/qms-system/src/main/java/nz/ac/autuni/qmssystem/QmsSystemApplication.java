b00e895dd799c93f7c6c261c678dbef6cf92922e
Simon <programmer0simon1han@gmail.com>
2020-09-03 09:48:50 +1200

    Create Backend Project
    Spring boot + MongoDB

---
+++ backend/qms-system/src/main/java/nz/ac/autuni/qmssystem/QmsSystemApplication.java
@@ -0,0 +1,13 @@
+package nz.ac.autuni.qmssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QmsSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QmsSystemApplication.class, args);
+	}
+
+}
---
+++ backend/qms-system/src/test/java/nz/ac/autuni/qmssystem/QmsSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package nz.ac.autuni.qmssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QmsSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
