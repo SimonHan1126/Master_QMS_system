6b40158fe9dc3d68b1cd1d05af4220b28a1f112e
Simon <programmer0simon1han@gmail.com>
2020-09-11 13:48:13 +1200

    Get ApproveController back

---
+++ backend/qms-system/src/main/java/nz/ac/autuni/qmssystem/controller/ApproveController.java
@@ -0,0 +1,37 @@
+package nz.ac.autuni.qmssystem.controller;
+
+import nz.ac.autuni.qmssystem.dao.FMEATableDao;
+import nz.ac.autuni.qmssystem.dao.RiskProcedureDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Simon-the-coder
+ * @date 10/09/20 10:34 pm
+ */
+
+@RestController
+@RequestMapping("/api/v1/approve")
+public class ApproveController {
+
+    @Autowired
+    private FMEATableDao fmeaTableService;
+    @Autowired
+    private RiskProcedureDao riskProcedureService;
+
+    @PostMapping("/approveFMEATable")
+    public ResponseEntity<Void> approveFMEATable(Long fmeaTableId) {
+        fmeaTableService.approveFMEATable(fmeaTableId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/removeFMEATable")
+    public ResponseEntity<Void> approveRiskProcedure(Long riskProcedureId) {
+        riskProcedureService.approveRiskProcedure(riskProcedureId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
