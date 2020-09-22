6b6414501e7fe45952892510705d7f0ccc741b1f
Simon <programmer0simon1han@gmail.com>
2020-09-23 00:14:40 +1200

    Update risk_procedure.dart

--- frontend/QMS_system/lib/model/risk_procedure.dart
+++ frontend/QMS_system/lib/model/risk_procedure.dart
@@ -1,16 +1,18 @@
 
 class RiskProcedure {
-  final int riskProcedureId;
+  final String riskProcedureId;
+  final String harm;
   final int severity;
   final String severityDescription;
   final int probability;
   final String probabilityDescription;
   final bool isApprove;
 
-  RiskProcedure(this.riskProcedureId, this.severity, this.severityDescription, this.probability, this.probabilityDescription, this.isApprove);
+  RiskProcedure(this.riskProcedureId, this.severity, this.severityDescription, this.probability, this.probabilityDescription, this.isApprove, this.harm);
 
   RiskProcedure.fromJson(Map<String, dynamic> json)
       : riskProcedureId = json['riskProcedureId'],
+        harm = json['harm'],
         severity = json['severity'],
         severityDescription = json['severityDescription'],
         probability = json['probability'],
@@ -20,6 +22,7 @@ class RiskProcedure {
   Map<String, dynamic> toJson() =>
       {
         'riskProcedureId': riskProcedureId,
+        'harm': harm,
         'severity': severity,
         'severityDescription': severityDescription,
         'probability': probability,
