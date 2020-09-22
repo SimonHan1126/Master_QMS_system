38f38d836dc8df52cb30f246a3449af9255e622a
Simon <programmer0simon1han@gmail.com>
2020-09-23 00:09:27 +1200

    add bloc files

---
+++ frontend/QMS_system/lib/bloc/bloc_provider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'bloc.dart';
+
+// 1
+class BlocProvider<T extends Bloc> extends StatefulWidget {
+  final Widget child;
+  final T bloc;
+
+  const BlocProvider({Key key, @required this.bloc, @required this.child})
+      : super(key: key);
+
+  // 2
+  static T of<T extends Bloc>(BuildContext context) {
+    // final type = _providerType<BlocProvider<T>>();
+    // final BlocProvider<T> provider = context.ancestorWidgetOfExactType(type);
+    final BlocProvider<T> provider = context.findAncestorWidgetOfExactType();
+    return provider.bloc;
+  }
+
+  // 3
+  static Type _providerType<T>() => T;
+
+  @override
+  State createState() => _BlocProviderState();
+}
+
+class _BlocProviderState extends State<BlocProvider> {
+  // 4
+  @override
+  Widget build(BuildContext context) => widget.child;
+
+  // 5
+  @override
+  void dispose() {
+    widget.bloc.dispose();
+    super.dispose();
+  }
+}
---
+++ frontend/QMS_system/lib/bloc/risk_procedure_bloc.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+
+import 'package:QMS_system/model/risk_procedure.dart';
+import 'package:QMS_system/util/api_base_helper.dart';
+import 'bloc.dart';
+
+class RiskProcedureBloc implements Bloc {
+  RiskProcedure _riskProcedure;
+  RiskProcedure get selectedRiskProcedure => _riskProcedure;
+
+  final _riskProcedureController = StreamController<RiskProcedure>();
+  final ApiBaseHelper _helper = ApiBaseHelper();
+
+  Stream<RiskProcedure> get riskProcedureStream => _riskProcedureController.stream;
+  StreamSink<RiskProcedure> get riskProcedureSink => _riskProcedureController.sink;
+
+  void submitQuery() async {
+    final response = await _helper.get("risk_procedure/findRiskProcedureById?riskProcedureId=78789");
+    _riskProcedureController.sink.add(RiskProcedure.fromJson(response));
+  }
+  
+  void saveRiskProcedure(riskProcedure) async {
+    print("this is RiskProcedureBloc saveRiskProcedure " + riskProcedure.toString());
+    final response = await _helper.post("risk_procedure/saveRiskProcedure", riskProcedure);
+    _riskProcedureController.sink.add(RiskProcedure.fromJson(response));
+  }
+
+  @override
+  void dispose() {
+    _riskProcedureController.close();
+  }
+}
