e3bd4c326325c6acf97bb781c53b12b459f25966
Simon <programmer0simon1han@gmail.com>
2020-09-23 00:16:29 +1200

    Update risk_procedure_expansion_panel.dart

--- frontend/QMS_system/lib/widget/risk_procedure/risk_procedure_expansion_panel.dart
+++ frontend/QMS_system/lib/widget/risk_procedure/risk_procedure_expansion_panel.dart
@@ -1,3 +1,6 @@
+import 'package:QMS_system/bloc/bloc_provider.dart';
+import 'package:QMS_system/bloc/risk_procedure_bloc.dart';
+import 'package:QMS_system/model/risk_procedure.dart';
 import 'package:QMS_system/widget/risk_procedure/risk_estimation_table.dart';
 import 'package:flutter/material.dart';
 
@@ -7,39 +10,101 @@ class RiskProcedureExpansionPanelWidget extends StatefulWidget {
 
 class RiskProcedureExpansionPanelWidgetState extends State<RiskProcedureExpansionPanelWidget> {
 
+  final _bloc = RiskProcedureBloc();
+
   bool _isExpanded = false;
 
-  List _subTitleList = ["Harm", "Severity","Severity Description", "Probability", "Probability Description"];
+  Map<String, TextEditingController>  _textEditingControllerMap = {};
+
+  Map<String, String> _subTitleMap = {
+    "harm" : "Harm",
+    "severity" : "Severity",
+    "severityDescription" : "Severity Description",
+    "probability" : "Probability",
+    "probabilityDescription" :  "Probability Description"
+  };
 
   @override
   void initState() {
     super.initState();
   }
 
-  List<Widget> _buildExpansionPanelItemList() {
+
+  @override
+  void dispose() {
+    super.dispose();
+    _bloc.dispose();
+  }
+
+  Widget _buildExpansionPanelItemList(RiskProcedure riskProcedure) {
     List<Widget> tiles = [];
-    for (var item in _subTitleList) {
-      tiles.add(
-        Card(
-          margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
-          child: Padding(
-            padding: EdgeInsets.all(8),
-            child: new TextFormField(
-              decoration: new InputDecoration(
-                  labelText: item,
-                  hintText: item,
-                  hintStyle: TextStyle(color: Colors.grey)
+    if(riskProcedure != null) {
+      Map<String, dynamic> mapRiskProcedure = riskProcedure.toJson();
+      mapRiskProcedure.forEach((key, value) {
+        value = value ?? "";
+        if (key.compareTo("riskProcedureId") != 0 && key.compareTo("isApprove") != 0) {
+          _textEditingControllerMap[key] = TextEditingController();
+          tiles.add(
+              Container(
+                padding: EdgeInsets.all(10),
+                child:  Text(_subTitleMap[key.toString()], style: TextStyle(fontWeight: FontWeight.bold)),
+              )
+          );
+          tiles.add(
+            Card(
+              margin: EdgeInsets.fromLTRB(10, 0, 0, 10),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: new TextFormField(
+                  controller: _textEditingControllerMap[key],
+                  decoration: new InputDecoration(
+                    labelText: value.toString(),
+                  ),
+                ),
               ),
             ),
-          ),
-        ),
-      );
+          );
+        }
+      });
     }
+
+
     tiles.add(RiskEstimationTable());
-    return tiles;
+
+    return ListBody(
+      children: tiles,
+    );
+  }
+
+  _saveRiskProcedure() async {
+    print("this is _saveRiskProcedure 1111111");
+
+    var riskProcedureObj = {
+      "riskProcedureId" : DateTime.now().millisecondsSinceEpoch.toString(),
+      "harm" : _textEditingControllerMap["harm"].text,
+      "severity" : _textEditingControllerMap["severity"].text,
+      "severityDescription" : _textEditingControllerMap["severityDescription"].text,
+      "probability" : _textEditingControllerMap["probability"].text,
+      "probabilityDescription" :  _textEditingControllerMap["probabilityDescription"].text
+      // "isApprove" : false
+    };
+    //
+    // RiskProcedure rp = RiskProcedure(
+    //     DateTime.now().millisecondsSinceEpoch.toString(),
+    //     int.parse(_textEditingControllerMap["severity"].text),
+    //     _textEditingControllerMap["severityDescription"].text,
+    //     int.parse(_textEditingControllerMap["probability"].text),
+    //     _textEditingControllerMap["probabilityDescription"].text,
+    //     false,
+    //     _textEditingControllerMap["harm"].text
+    // );
+    print("this is _saveRiskProcedure 2222 " + riskProcedureObj.toString());
+    _bloc.saveRiskProcedure(riskProcedureObj);
   }
 
   Widget build(BuildContext context) {
+
+    _bloc.submitQuery();
     return Container(
       alignment: Alignment.center,
       child: Column(
@@ -49,7 +114,7 @@ class RiskProcedureExpansionPanelWidgetState extends State<RiskProcedureExpansio
               ExpansionPanel(
                 headerBuilder: (context, isExpanded) {
                   return ListTile(
-                    title: Text("Risk Procedure", style: TextStyle(fontWeight: FontWeight.bold)),
+                    title: Text("Risk Procedure", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
                     trailing: Row(
                       mainAxisSize: MainAxisSize.min,
                       children: <Widget>[
@@ -60,7 +125,7 @@ class RiskProcedureExpansionPanelWidgetState extends State<RiskProcedureExpansio
                             color: Color(0xFF50AFC0),
                           ),
                           onPressed: () {
-                            print("this is save Button");
+                            _saveRiskProcedure();
                           },
                         ),
                         IconButton(
@@ -79,8 +144,15 @@ class RiskProcedureExpansionPanelWidgetState extends State<RiskProcedureExpansio
                 },
                 body: Padding(
                   padding: EdgeInsets.fromLTRB(15, 10, 15, 15),
-                  child: ListBody(
-                    children: _buildExpansionPanelItemList()
+                  // child: _buildExpansionPanelItemList(null),
+                  child: BlocProvider<RiskProcedureBloc>(
+                    bloc: _bloc,
+                    child: StreamBuilder(
+                      stream: _bloc.riskProcedureStream,
+                      builder: (context, snapshot) {
+                        return _buildExpansionPanelItemList(snapshot.data);
+                      },
+                    )
                   ),
                 ),
                 isExpanded: _isExpanded,
